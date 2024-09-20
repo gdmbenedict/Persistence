cead6b83db9a1b9d7f03b1258e64de6bb47e0f04
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2024-09-20 09:03:47 -0300

    Level Manager
    created a manager to manage the levels and switch scenes.

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
@@ -15,10 +16,37 @@ public class LevelManager : MonoBehaviour
     [Header("Object Reference")]
     [SerializeField] private UIManager uiManager;
 
+    private Level activeLevel;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        switch (SceneManager.GetActiveScene().name)
+        {
+            case "MainMenu":
+                uiManager.ChangeScreen(UIManager.UIScreen.mainMenu);
+                activeLevel = Level.mainMenu;
+                break;
+
+            case "Level01":
+                uiManager.ChangeScreen(UIManager.UIScreen.charCreate);
+                activeLevel = Level.level1;
+                break;
+
+            case "Level02":
+                uiManager.ChangeScreen(UIManager.UIScreen.gamePlay);
+                activeLevel = Level.level2;
+                break;
+
+            case "Level03":
+                uiManager.ChangeScreen(UIManager.UIScreen.shop);
+                activeLevel = Level.level3;
+                break;
+
+            default:
+                ChangeLevel(Level.mainMenu);
+                break;
+        }
     }
 
     // Update is called once per frame
@@ -32,20 +60,33 @@ public class LevelManager : MonoBehaviour
         switch (targetLevel)
         {
             case Level.mainMenu:
+                SceneManager.LoadScene("MainMenu");
                 uiManager.ChangeScreen(UIManager.UIScreen.mainMenu);
+                activeLevel = Level.mainMenu;
                 break;
 
             case Level.level1:
+                SceneManager.LoadScene("Level01");
                 uiManager.ChangeScreen(UIManager.UIScreen.charCreate);
+                activeLevel = Level.level1;
                 break;
 
             case Level.level2:
+                SceneManager.LoadScene("Level02");
                 uiManager.ChangeScreen(UIManager.UIScreen.gamePlay);
+                activeLevel = Level.level2;
                 break;
 
             case Level.level3:
+                SceneManager.LoadScene("Level03");
                 uiManager.ChangeScreen(UIManager.UIScreen.shop );
+                activeLevel = Level.level3;
                 break;
         }
+    }
+
+    public Level GetLevel()
+    {
+        return activeLevel;
     }
 }
